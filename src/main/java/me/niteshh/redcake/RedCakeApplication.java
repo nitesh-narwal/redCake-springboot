2d0073d1e2fbbb4391e49083808aebfe3f68e4b9
Nitesh <nnehrwal2002@gmail.com>
2026-09-14 19:52:11 +0530

    created sockets

---
+++ src/main/java/me/niteshh/redcake/RedCakeApplication.java
@@ -0,0 +1,13 @@
+package me.niteshh.redcake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedCakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedCakeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/niteshh/redcake/RedCakeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.niteshh.redcake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedCakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
